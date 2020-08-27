4fc2a70c8c8dc7e82d6cb44cc32d6a4bb1a85f09
Gabriel Nogueira <gabrielnoog7@gmail.com>
2020-08-27 20:41:10 -0300

    Importando PathVariable

--- aula01-spring-boot/src/main/java/com/example/aula01springboot/controller/ClienteController.java
+++ aula01-spring-boot/src/main/java/com/example/aula01springboot/controller/ClienteController.java
@@ -1,6 +1,7 @@
 package com.example.aula01springboot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
